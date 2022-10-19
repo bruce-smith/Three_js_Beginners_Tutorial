72a4f93e2efea5e6db505abb62e781a6c1eef0be
Bruce Smith <brucesmith97@outlook.com>
2022-10-19 13:57:36 -0500

    7:11

--- main.js
+++ main.js
@@ -10,8 +10,27 @@ const renderer = new THREE.WebGLRenderer({
 	canvas: document.querySelector('#bg'),
 });
 
-renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth , window.innerHeight);
 camera.position.setZ(30);
+camera.position.setX(-3);
 
 renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true});
+const torus = new THREE.Mesh(geometry,material);
+
+scene.add(torus);
+
+function animate()
+{
+	requestAnimationFrame(animate);
+	
+	torus.rotation.x += 0.01;
+	torus.rotation.x += 0.005;
+	torus.rotation.x += 0.01;
+	renderer.render(scene, camera);
+}
+
+animate();
